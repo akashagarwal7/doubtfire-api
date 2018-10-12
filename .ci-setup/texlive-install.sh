68bdc79c9e850175fca720000a687400d3c045bd
Akash Agarwal <akashaggarwal777@gmail.com>
2018-10-12 17:32:05 +1100

    CONFIG: Add lualatex PATH to tex-live install script and travis

--- .ci-setup/texlive-install.sh
+++ .ci-setup/texlive-install.sh
@@ -3,8 +3,8 @@
 APP_PATH=`echo $0 | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
 APP_PATH=`cd "$APP_PATH"; pwd`
 
-# See if there is a cached version of TL available
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+# See if there is a cached version of TL available
 if ! command -v lualatex > /dev/null; then
   # Obtain TeX Live
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
